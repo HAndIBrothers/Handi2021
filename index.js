59ab464cebc40891ccc3b6ea3ba89df9db443ad2
Lee In Jae <risnworlds@gmail.com>
2021-08-20 17:37:47 +0900

    2021-08-20 테스트
    - Test

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-﻿function test(text) {
-    alert(text);
+﻿//#region Open / Close
+var gold;
+var autoGold;
+function Open() {
+    this.gold = 0;
+    this.autoGold = 1;
+    this.UpdateGold();
 }
+//#endregion
+
+//#region Add Gold
+function AddGold(addGold) {
+    this.gold += addGold;
+    this.UpdateGold();
+} 
+var doAuto = false;
+var interAuto;
+function AddGold_Auto() {
+    this.doAuto = !this.doAuto;
+
+    if(this.doAuto) {
+        this.interAuto = this.setInterval(() => {
+            this.AddGold(this.autoGold);
+        }, 100);
+    } else {
+        clearInterval(this.interAuto);
+    }
+} 
+//#endregion
+
+//#region Update Gold
+function UpdateGold() {
+    var TEXT_Gold = document.getElementById('item_gold');
+    TEXT_Gold.innerHTML = this.gold;
+}
+//#endregion
