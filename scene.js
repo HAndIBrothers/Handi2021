b6b537d15922c711ff6c0c13e4cdc6d0c200ab35
Lee In Jae <>
2021-09-05 13:58:52 +0900

    rabbits added

---
+++ scene.js
@@ -0,0 +1,18 @@
+﻿function openScene() {
+    moveRabbit();
+}
+
+function moveRabbit() {
+    let rabbits = document.querySelector('.field_image_bottom');
+    let check = true;
+    let timerId = setInterval(() => {
+        if(check) {
+            rabbits.style.justifyContent= "space-evenly";
+            check = false;
+        } else {
+            rabbits.style.justifyContent= "center";
+            check = true;
+        }
+    }, 1000);
+}
+
